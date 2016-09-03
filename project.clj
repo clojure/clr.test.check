6f2a4f1a0bbfaf67979e6a5089f67304b157779f
David Miller <dmiller2718@gmail.com>
2016-09-03 14:27:42 -0500

    Update to Java commit 1dc1b6a (2015.09.12): prepare for next development iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure.clr/test.check "0.8.2"
+(defproject org.clojure.clr/test.check "0.9.0-SNAPSHOT"
   :description "Port of github.com/clojure/test.check to ClojureCLR"
   :url "https://github.com/clojure/clr.test.check"
   :license {:name "Eclipse Public License"
