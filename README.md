da5a297d0e9406bf72fe2feffe327b60c7719d22
David Miller <dmiller2718@gmail.com>
2016-09-11 15:00:43 -0500

    Update to Java commit f8b1c96 (2015.11.12): prepare release teset.check-0.9.0

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ Nuget reference:
 
 Leiningen/Clojars reference:
 
-   [org.clojure.clr/test.check "0.8.2"]
+   [org.clojure.clr/test.check "0.9.0"]
 
    
 ## Notes on the ported code
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure.clr/test.check "0.9.0-SNAPSHOT"
+(defproject org.clojure.clr/test.check "0.9.0"
   :description "Port of github.com/clojure/test.check to ClojureCLR"
   :url "https://github.com/clojure/clr.test.check"
   :license {:name "Eclipse Public License"
